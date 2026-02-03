6168e2e97e032295a66797ff905357effcfc248e
Liam B. <liam.berge72@gmail.com>
2026-02-03 15:35:41 +0100

    Added: shared library of includes/imports
    - Added shared.h that enables me to not import the same 36 includes for
    each file that needs the same 36 include.

--- src/main.c
+++ src/main.c
@@ -1,26 +1,14 @@
-#include "../thirdparty/SDL3/include/SDL3/SDL.h"
 #include "../thirdparty/SDL3/include/SDL3/SDL_init.h"
 #include "../thirdparty/SDL3/include/SDL3/SDL_error.h"
-#include <SDL3/SDL_log.h>
-#include <stdbool.h>
 
-#define NOB_IMPLEMENTATION
-#include "../thirdparty/nob.h"
-
-#define FLAG_IMPLEMENTATION
-#include "../thirdparty/flag.h"
+#include "../shared.h"
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
-int main(int argc, char *argv[])
+int main()
 {
-    bool *autoQuit = flag_bool("-auto-quit", false, "Auto quits the application, useful for testing");
     int result = 0;
-    if (!flag_parse(argc, argv)) {
-        flag_print_error(stderr);
-        return_defer(1);
-    }
 
     if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS)) {
         nob_log(ERROR, "SDL failed to initialize. See: %s", SDL_GetError());
