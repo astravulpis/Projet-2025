4911b23ce9dacd951fd653bc498c0454595bfbfb
Liam B. <liam.berge72@gmail.com>
2026-02-04 10:54:44 +0100

    Feat: removing out-of-scope feature from the branch

--- src/main.c
+++ src/main.c
@@ -8,25 +8,15 @@
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
-typedef struct sdl_context_s sdl_context_t;
 static struct sdl_context_s {
     SDL_Window *window; //< SDL3 window context
     SDL_Surface *renderingSurface; //< SDL3 surface context
 } sdl_context = {0};
 
-SDL_Surface *WINDOW_IMAGE_TEST = NULL;
-
 /**
  * fonction qui initialiste tout les sous modules de SDL3 qui vont être utilisés
  */
 bool init_all(void);
-
-/**
- * charge une image en recevant son chemin et un pointeur de type SDL_surface
- * @var chemin Chemin de l'image charger
- * @var surface Surface qui va recevoir l'image
- */
-bool chargerImage(char *chemin, SDL_Surface *surface);
 
 /**
  * @brief fonction qui effectue toutes les désallocation et mise a NULL des pointeurs SDL3
@@ -39,16 +29,10 @@ int main()
 {
     init_all();
 
-    // if(chargerImage("assets/img/SDL3.bmp", WINDOW_IMAGE_TEST)==true)
-    //     printf("\n| chargement de l'image réussi\n");
-
-    //met l'image dans la surface WINDOW_SURFACE_CONTEXT
-    // SDL_BlitSurface(WINDOW_IMAGE_TEST, NULL, sdl_context.renderingSurface, NULL);
-
     bool quitterBool = false; //< booleen qui determine si la page doit continuer  s'afficher
     SDL_Event evenement;
 
-    float rgb = 24.0f/255.0f;
+    float color = 0x18/255.0f;
 
     while ( !quitterBool ){
 
@@ -57,9 +41,8 @@ int main()
             case SDL_EVENT_QUIT: quitterBool=true;
         }
 
-        SDL_ClearSurface(sdl_context.renderingSurface, rgb, rgb, rgb, 1.0f);
-        // SDL_BlitSurface(WINDOW_IMAGE_TEST, NULL, sdl_context.renderingSurface, NULL);
-        SDL_UpdateWindowSurface( sdl_context.window );
+        SDL_ClearSurface(sdl_context.renderingSurface, color, color, color, 1.0f);
+        SDL_UpdateWindowSurface(sdl_context.window);
     }
 
     close_SDL();
@@ -74,7 +57,7 @@ bool init_all(void)
         return false;
     }
 
-    sdl_context.window = SDL_CreateWindow("ULTRACOOL", WINDOW_WIDTH, WINDOW_HEIGHT, 0);
+    sdl_context.window = SDL_CreateWindow("ULTRAC00L", WINDOW_WIDTH, WINDOW_HEIGHT, 0);
     if (sdl_context.window == NULL) {
         nob_log(ERROR, "SDL failed to initialize. See: %s", SDL_GetError());
         close_SDL();
@@ -85,22 +68,10 @@ bool init_all(void)
     return true;
 }
 
-bool chargerImage(char *chemin, SDL_Surface *surface)
-{
-    surface = SDL_LoadBMP(chemin);
-
-    if (surface == NULL) {
-        nob_log(ERROR, "Impossible de charger l'image %s! cause: %s\n", chemin, SDL_GetError());
-        return false;
-    }
-    return true;
-}
-
 void close_SDL()
 {
     SDL_DestroyWindow(sdl_context.window);
 
-    WINDOW_IMAGE_TEST = NULL;
     sdl_context.window = NULL;
     sdl_context.renderingSurface = NULL;
 
