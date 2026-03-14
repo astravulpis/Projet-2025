c6060b28fc897f53b3bfda974860cfa004b28df9
Liam B. <liam.berge72@gmail.com>
2026-03-14 16:00:42 +0100

    Feat: added slamming + sliding down

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
-#include <raylib.h>
 #include "../shared.h"
+#include <raylib.h>
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
@@ -13,8 +13,6 @@ typedef struct movementKey_s {
     KeyboardKey right; //!< Right movement key
     KeyboardKey up;    //!< Up movement key
     KeyboardKey down;  //!< Down movement key
-    KeyboardKey rotLeft;  //!< Down movement key
-    KeyboardKey rotRight;  //!< Down movement key
 } keymap;
 
 /**
@@ -31,16 +29,22 @@ typedef enum layoutType_s {
  * Access via keySchema[LAYOUT_QWERTY], etc.
  */
 static keymap keyLayout[] = {
-    [LAYOUT_QWERTY] = (keymap){KEY_A, KEY_D, KEY_W, KEY_S, KEY_Q, KEY_E},
-    [LAYOUT_AZERTY] = (keymap){KEY_Q, KEY_D, KEY_Z, KEY_S, KEY_A, KEY_E},
+    [LAYOUT_QWERTY] = (keymap){.left = KEY_A, .right = KEY_D, .up = KEY_SPACE, .down = KEY_LEFT_SHIFT},
+    [LAYOUT_AZERTY] = (keymap){.left = KEY_Q, .right = KEY_D, .up = KEY_SPACE, .down = KEY_LEFT_SHIFT},
 };
 
 int main(int argc, char *argv[])
 {
     float rot = 0.0f;
     Vector2 imgPos = {200, 200};
-    float scale = 0.15f;
-    int movementSpeed = 5;
+    float scale = 0.05f;
+    float movementSpeed = 150.0f;
+    Vector2 vel = {0};
+    float gravity = 9.2f;
+    float jumpForce = -4.5f;
+    KeyboardKey lastDirection = KEY_NULL;
+    float stunnedTimer = 0.0f;
+    bool onGround = false;
 
     bool *azertyActive = flag_bool("azertySchema", false, "Use the azerty layout schema");
     keymap activeLayout = keyLayout[LAYOUT_QWERTY];
@@ -51,52 +55,71 @@ int main(int argc, char *argv[])
         activeLayout = keyLayout[LAYOUT_AZERTY];
     }
 
-
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "ULTRAC00L - Window Display");
-    SetTargetFPS(60);
+    SetTargetFPS(144);
     Texture2D logoC = LoadTexture("./assets/img/logoC.png");
 
+    size_t mark = temp_save();
+
     while (!WindowShouldClose()) {
-        // Rotation
-        if (IsKeyDown(activeLayout.rotLeft)) {
-            rot = (rot >= 360) ? -360 : rot + 1;
-        } else if (IsKeyDown(activeLayout.rotRight)) {
-            rot = (rot <= -360) ? 360 : rot - 1;
+        temp_rewind(mark);
+
+        if (stunnedTimer > 0) {
+            stunnedTimer = (stunnedTimer <= 0) ? 0 : stunnedTimer - 0.25f * GetFrameTime();
         }
+
+        if (imgPos.y + logoC.height * scale >= WINDOW_HEIGHT) {
+            onGround = true;
+            imgPos.y = WINDOW_HEIGHT - logoC.height * scale;
+            vel.y = 0;
+        } else {
+            onGround = false;
+            stunnedTimer = 0.025;
+        }
+
+        if (IsKeyDown(activeLayout.up) && onGround) {
+            vel.y = jumpForce;
+        }
+
+        vel.y += gravity * GetFrameTime();
+        imgPos.y += vel.y;
 
         // Vertical Movement
         if (IsKeyDown(activeLayout.down)) {
-            imgPos.y = (imgPos.y+logoC.height*scale >= WINDOW_HEIGHT) ? WINDOW_HEIGHT - logoC.height*scale : imgPos.y + movementSpeed;
-        } else if (IsKeyDown(activeLayout.up)) {
-            imgPos.y = (imgPos.y <= 0) ? 0 : imgPos.y - movementSpeed;
+            if (!onGround) {
+                imgPos.y = (imgPos.y + logoC.height * scale >= WINDOW_HEIGHT) ? WINDOW_HEIGHT - logoC.height * scale
+                                                                              : imgPos.y - jumpForce * 1.45f;
+            } else {
+                if (stunnedTimer <= 0.0f) {
+                    if (lastDirection == KEY_NULL || lastDirection == activeLayout.right) {
+                        imgPos.x = (imgPos.x + logoC.width * scale >= WINDOW_WIDTH) ? WINDOW_WIDTH - logoC.width * scale
+                                                                                    : imgPos.x + (movementSpeed * 2) * GetFrameTime();
+
+                    } else if (lastDirection == activeLayout.left) {
+                        imgPos.x = (imgPos.x <= 0) ? 0 : imgPos.x - (movementSpeed * 2) * GetFrameTime();
+                    }
+                }
+            }
         }
 
         // Horizontal Movement
         if (IsKeyDown(activeLayout.right)) {
-            imgPos.x = (imgPos.x+logoC.width*scale >= WINDOW_WIDTH) ? WINDOW_WIDTH - logoC.width*scale : imgPos.x + movementSpeed;
+            imgPos.x = (imgPos.x + logoC.width * scale >= WINDOW_WIDTH) ? WINDOW_WIDTH - logoC.width * scale
+                                                                        : imgPos.x + movementSpeed * GetFrameTime();
+            lastDirection = activeLayout.right;
         } else if (IsKeyDown(activeLayout.left)) {
-            imgPos.x = (imgPos.x <= 0) ? 0 : imgPos.x - movementSpeed;
+            imgPos.x = (imgPos.x <= 0) ? 0 : imgPos.x - movementSpeed * GetFrameTime();
+            lastDirection = activeLayout.left;
         }
 
-        BeginDrawing(); { // Note: Les `{` et `}` ne sont pas obligatoires. Elles ne sont que pour un formattage plus comprehensible
-                          //       de "qu'est ce qui se passe a cette endroit". Si elles vous genent, vous pouvez les enlever.
-            ClearBackground(GetColor(0x6495EDFF));
+        BeginDrawing();
+        {
+            ClearBackground(GetColor(0x181818FF));
             DrawTextureEx(logoC, imgPos, rot, scale, WHITE);
-        } EndDrawing();
-
-#if 0
-        // Une simple indentation peut aussi fonctionner.
-        BeginDrawing();
-            ClearBackground(GetColor(0x6495EDFF));
+            DrawText(temp_sprintf("x: %.2f, y: %.2f", imgPos.x, imgPos.y), 10, 10, 18, WHITE);
+            DrawText(temp_sprintf("stunned: %.2f", stunnedTimer), 10, 24, 18, WHITE);
+        }
         EndDrawing();
-#endif // 0
-
-#if 0
-        // Ou juste laisser le tout sur la meme colonne.
-        BeginDrawing();
-        ClearBackground(GetColor(0x6495EDFF));
-        EndDrawing();
-#endif // 0
     }
     CloseWindow();
     return 0;
