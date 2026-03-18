ec70f68dc4e072cce800c8b4e1218031aca28cb9
Rossignol François <francois_rossignol@outlook.fr>
2026-03-18 19:32:00 +0100

    Ajout d'un bouton en dur dans le main pour le background de bouton texte affiché dans la box du bouton changement de place du SDL_GetMouseState, pour pouvoir l'utiliser a l'avenir pour que le bouton soit interractif remplissage d'arrière plan via RenderDrawColor, pas de tecture pour le background des boutons

--- src/main.c
+++ src/main.c
@@ -29,6 +29,11 @@ int main()
     SDL_FRect *boxDummy = createRect(400.0f, 400.0f, 50.0f, 50.0f);
     SDL_Texture *logoC = IMG_LoadTexture(sdl_ctx->renderer, "./assets/img/C.png");
 
+    SDL_FRect *boxBouton1 = createRect(WINDOW_WIDTH/2-150, WINDOW_HEIGHT/2, 300.0f, 75.0f);
+    V2f bouton1Pos = {boxBouton1->x+35, boxBouton1->y+20};
+
+    printf("x = %.2f\ny = %.2f\nw = %.2f\nh = %.2f\n", boxBouton1->x, boxBouton1->y, boxBouton1->w, boxBouton1->h);
+
     if (logoC == NULL) {
         nob_log(ERROR, "%s:%d: Failed to load image. See error: %s", __FILE__, __LINE__, SDL_GetError());
         destroyPlayer(&player);
@@ -71,13 +76,23 @@ int main()
         basicKeyboardEvents(sdl_ctx);
         UpdatePlayer(player, boxDummy, deltaT);
 
+        SDL_GetMouseState(&mouse_X, &mouse_Y);//je l'ai bougé car il semble plus adapté ici
         SDL_RenderClear(sdl_ctx->renderer);
         renderBackground(sdl_ctx);
+        
+        //pour la zone de bouton c'est assez rapide de faire comme ça : on met une couleur de rendu, on rend dans la box bouton, avec la couleur mise
+        //ensuite on remet en noir pour que le background soit noir a la prochaine itération
+        SDL_SetRenderDrawColor(sdl_ctx->renderer, 0, 0, 255, 255);
+        SDL_RenderFillRect(sdl_ctx->renderer, boxBouton1);
+        SDL_SetRenderDrawColor(sdl_ctx->renderer, 0, 0, 0, 255);
+
+        //affichage du texte du boutton (dans la même box)
+        renderText_Ex(sdl_ctx, temp_sprintf("bouton"), WHITE, bouton1Pos);
+        
 
         renderPlayer(player);
         renderImage(sdl_ctx, logoC, boxDummy);
 
-        SDL_GetMouseState(&mouse_X, &mouse_Y);
         renderText_Ex(sdl_ctx, temp_sprintf("Mouse: {%.1f, %.1f}", mouse_X, mouse_Y), WHITE, MouseTextPos);
         renderText_Ex(sdl_ctx, temp_sprintf("fps : %i", frameRate), WHITE, fpsTextPos);
         // renderText_Ex(sdl_ctx, temp_sprintf("timer: %.1f", player->stunnedTimer), WHITE, (V2f){10.0f, 148.0f});
