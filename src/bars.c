f3c543ba5f0c23538c4e2436c65f0bb3a7735d4d
Rossignol François <francois_rossignol@outlook.fr>
2026-04-07 16:39:15 +0200

    ajout attributs au type bar : - minCursorWidth pour restreindre la taille min du curseur d'une barre - displayHpValue pour choisir ou non d'afficher le texte en plus la jauge de la bare
    modifs du rendu et la création en conséquence de cela

--- src/bars.c
+++ src/bars.c
@@ -1,9 +1,10 @@
 #include "bars.h"
 #include "common.h"
 #include "sdl_helpers.h"
+#include <math.h>
 
 bool createBar(bar **h, SDL_FRect rect, SDL_Color bgColor, SDL_Color fillColor, SDL_Color cursorColor, float maxHp,
-               float barPadding)
+               float barPadding, float minCursorWidth, bool displayHpValue)
 {
     *h = calloc(1, sizeof(bar));
     if ((*h) == NULL) {
@@ -18,6 +19,9 @@ bool createBar(bar **h, SDL_FRect rect, SDL_Color bgColor, SDL_Color fillColor, 
 
     (*h)->maxHp = maxHp;
     (*h)->barPadding = barPadding;
+    (*h)->minCursorWidth = minCursorWidth; // doit avoir subis un ratio
+
+    (*h)->displayHpValue = displayHpValue; // par défaut hpValue sera affiché en numérique en plus de la jauge
 
     return true;
 }
@@ -33,26 +37,13 @@ void destroyBar(bar **h)
 void barRender(sdl_ctx_t *sdl_ctx, bar *h, float hpValue, int s_intensity, int l_intensity,
                int ls_opacity) // s = shadow et l = ligth
 {
-    int textWidth = 0;
-    int textHeight = 0;
-
-    // Compare hpValue a la valeur maximum de la barre, copie la valeur das hp, la rabaisse si trop grande, la met a 0 si
-    // négative cela ne se fait que sur la jauge, pas le pourcentage affiché (cela me parraît utile d'avoir la vrai valeur qui
+    // Compare hpValue a la valeur maximum de la barre, copie la valeur dans hp, la rabaisse si trop grande, la met a 0 si
+    // négative cela ne se fait que sur la jauge, pas le nombre affiché (cela me parraît utile d'avoir la vrai valeur qui
     // est mise en paramètre pour un potentiel debug)
     float hp = hpValue;
     if (hpValue > h->maxHp) hp = h->maxHp;
     else if (hpValue < 0)
         hp = 0;
-
-    char *hpText = temp_sprintf("%.1f%%", hpValue);
-    TTF_GetStringSize(sdl_ctx->font, hpText, 0, &textWidth, &textHeight);
-
-    // Calculates the center of the life point information text
-    float XCentering = ((h->BarBox)->w / 2.0f) - textWidth / 2.0f;
-    float YCentering = ((h->BarBox)->h / 2.0f) - textHeight / 2.0f;
-    V2f barTextPos = {(h->BarBox)->x + XCentering, (h->BarBox)->y + YCentering};
-    V2f barTextPosShadow = {(h->BarBox)->x + XCentering + 2 * sdl_ctx->screenRatio,
-                            (h->BarBox)->y + YCentering + 1 * sdl_ctx->screenRatio};
 
     // Calcul de fillBox via barPadding (normalement plus petite que h->BarBox, sinon il y'a un problème XD), largeur
     // calculées en fonction de hp, représente la jauge de vie
@@ -91,10 +82,10 @@ void barRender(sdl_ctx_t *sdl_ctx, bar *h, float hpValue, int s_intensity, int l
         (ls_opacity >= 0 && ls_opacity <= 255) ? (ls_opacity * (hp / h->maxHp))
                                                : 255}; // si l'opacité n'est pas bonne, alors l'ombre n'aura aucune transparence
 
-    // initialisations des boxs et couleurs pour le curseur et ses ombres/ lumières
+    // initialisations des boxs et couleurs pour le curseur et ses ombres/ lumières (fmaxf est utilisée pour être sûr que la largeur soit toujours supérieure ou égale a minCursorWidth)
     SDL_FRect cursorBox = {fillBox.x + fillBox.w - (h->barPadding * sdl_ctx->screenRatio / 2),
-                           h->BarBox->y + (6 * sdl_ctx->screenRatio) / 2, h->barPadding * sdl_ctx->screenRatio / 2,
-                           h->BarBox->h - (6 * sdl_ctx->screenRatio)};
+                           fillBox.y, fmaxf(h->barPadding * sdl_ctx->screenRatio / 2, h->minCursorWidth),
+                           fillBox.h};
 
     SDL_FRect cursorShadowBox = {cursorBox.x, cursorBox.y, cursorBox.w / 3, cursorBox.h};
     SDL_Color cursorShadowColor = {50, 50, 50, 100}; // valeurs en dur, je vais peut être revenir dessus, mais il me semble que
@@ -120,7 +111,26 @@ void barRender(sdl_ctx_t *sdl_ctx, bar *h, float hpValue, int s_intensity, int l
     renderFillRect(sdl_ctx->renderer, &cursorShadowBox, cursorShadowColor);
     renderFillRect(sdl_ctx->renderer, &cursorLightBox, cursorLightColor);
 
-    // rendu du texte, avec un effet d'ombre qui imite a la perfection le raytracing
-    renderText_Ex(sdl_ctx, hpText, BLACK, barTextPosShadow);
-    renderText_Ex(sdl_ctx, hpText, WHITE, barTextPos);
+    if (h->displayHpValue) { // affiche hpValue en numérique en plus de la jauge si le booléen est vrai
+        int textWidth = 0;
+        int textHeight = 0;
+
+        size_t mark = temp_save();
+        char *hpText = temp_sprintf("%.1f", hpValue);
+        temp_rewind(mark);
+
+        TTF_GetStringSize(sdl_ctx->font, hpText, 0, &textWidth, &textHeight);
+
+        // Calculates the center of the life point information text
+        float XCentering = ((h->BarBox)->w / 2.0f) - textWidth / 2.0f;
+        float YCentering = ((h->BarBox)->h / 2.0f) - textHeight / 2.0f;
+
+        V2f barTextPos = {(h->BarBox)->x + XCentering, (h->BarBox)->y + YCentering};
+        V2f barTextPosShadow = {(h->BarBox)->x + XCentering + 2 * sdl_ctx->screenRatio,
+                            (h->BarBox)->y + YCentering + 1 * sdl_ctx->screenRatio};        
+
+        // rendu du texte, avec un effet d'ombre qui imite a la perfection le raytracing
+        renderText_Ex(sdl_ctx, hpText, BLACK, barTextPosShadow);
+        renderText_Ex(sdl_ctx, hpText, WHITE, barTextPos);
+    }
 }
--- src/bars.h
+++ src/bars.h
@@ -23,10 +23,13 @@ typedef struct bar {
 
     float maxHp;      // pour avoir le bon ratio dans la jauge
     float barPadding; // proportion que la jauge prend dans BarBox
+    float minCursorWidth;
+
+    bool displayHpValue; // booléen qui permet d'indiquer si l'on doit afficher hpValue au rendu ou pas
 } bar;
 
 bool createBar(bar **h, SDL_FRect rect, SDL_Color bgColor, SDL_Color fillColor, SDL_Color cursorColor, float maxHp,
-               float barPadding);
+               float barPadding, float minCursorWidth, bool displayHpValue);
 void destroyBar(bar **h);
 
 void barRender(sdl_ctx_t *sdl_ctx, bar *h, float hpValue, int s_intensity, int l_intensity, int ls_opacity);
