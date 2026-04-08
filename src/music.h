55369e010fa7bebb5bfb80f37be5a9bab0e5341c
Reeves Guillaume <greeves2306@gmail.com>
2026-04-08 16:27:14 +0200

    adding music and sound documentation

--- src/music.h
+++ src/music.h
@@ -3,12 +3,22 @@
 
 #include "common.h"
 
+/**
+ * @struct sfx
+ * @brief sound effect details
+ */
 typedef struct {
     char *name;
     MIX_Audio *ptr;
     size_t track_idx;
 } sfx;
 
+/**
+ * @struct sfxs
+ * @brief list of sound effects
+ *
+ * contains a list of individual sound effects
+ */
 typedef struct {
     sfx **items;
     size_t count;
@@ -16,16 +26,20 @@ typedef struct {
 } sfxs;
 
 /**
- * @fn Mix_Init(const char * path, sdl_ctx_t * ctx)
+ * @fn loadTrack(sdl_ctx_t *ctx, int trackIdx, const char *path)
  * @param[in] path pointer pointer to the player itself
  * @param[in] ctx our own sdl context variable
- * @brief initialises the level music
- *
- * takes the path to the music obtained in the level parsing. this is then fed into SDL_Mixer to
- * start playing music for the level
+ * @param[in] trackIdx
+ * @brief loads a track into ctx
  */
 bool loadTrack(sdl_ctx_t *ctx, int trackIdx, const char *path);
 
+/**
+ * @fn playTrack(sdl_ctx_t *ctx, int trackIdx)
+ * @param[in] ctx our own sdl context variable
+ * @param[in] trackIdx id of the track we want to play
+ * @brief plays the track corresponding to the track ID
+ */
 void playTrack(sdl_ctx_t *ctx, int trackIdx);
 
 /**
@@ -43,10 +57,36 @@ void playTrack(sdl_ctx_t *ctx, int trackIdx);
  */
 void loadSfx(sdl_ctx_t *sdl_ctx, sfxs *audios, size_t track_name, char *name, const char *path);
 
+/**
+ * @fn playSfx(sdl_ctx_t *ctx, sfxs *audios, const char *sfx_name)
+ * @param[in] ctx our own sdl context variable
+ * @param[in] audios list of all the audios
+ * @param[in] sfx_name name of the audio we are looking for
+ * @brief goes through the list of sound effects looking for the one with the given name
+ */
 void playSfx(sdl_ctx_t *ctx, sfxs *audios, const char *sfx_name);
+
+/**
+ * @fn __playSfx(sdl_ctx_t *sdl_ctx, size_t track_idx, MIX_Audio *audio)
+ * @param[in] ctx our own sdl context variable
+ * @param[in] track_idx id of the track we want to play
+ * @param[in] audio audio pointer to MIX
+ * @brief internal function to seperate the setting track audio and actually playing it
+ */
 void __playSfx(sdl_ctx_t *sdl_ctx, size_t track_idx, MIX_Audio *audio);
 
+/**
+ * @fn destroySfx(sfx **sfx)
+ * @param[in] sfx one sound effect
+ * @brief destroys a sound effect
+ */
 void destroySfx(sfx **sfx);
+
+/**
+ * @fn destroySfxs(sfxs *sfxs)
+ * @param[in] sfxs list of all the sound effects
+ * @brief goes through the list of all sound effects to destory them all
+ */
 void destroySfxs(sfxs *sfxs);
 
 #endif
