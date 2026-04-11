cf071ed4308a34c89da186615ed63ed00d5c11e6
Rossignol François <francois_rossignol@outlook.fr>
2026-04-11 11:18:09 +0200

    buttons.h et chexkboxes : masterclass de documentation

--- src/buttons.h
+++ src/buttons.h
@@ -2,11 +2,11 @@
  * @file button.h
  * @brief File to define what a button is and how it works
  *
- * Author: Rossignol François <francois_rossignol@outlook.fr>
- * Last Modified: 2026-03-26
- * Date: 2026-03-22
+ * @author Rossignol François <francois_rossignol@outlook.fr>
+ * @date 2026-03-19
+ * @remark last modified 2026-03-31 (added image support)
  *
- * * Contributors:
+ * Contributors:
  * Rossignol François <francois_rossignol@outlook.fr>
  **/
 
@@ -14,19 +14,6 @@
 #define BUTTONS_H_
 
 #include "common.h"
-
-/* Ancienne version du type boutons
-typedef struct button {
-    char *buttonText;
-    SDL_FRect *buttonBox;
-    SDL_Color baseColor;
-    SDL_Color hoverColor;
-    SDL_Color clickColor;
-    bool isHovered;
-    bool isLeftClicked;
-    bool isRightClicked;
-} button;
-*/
 
 /**
  * @struct button
@@ -46,17 +33,17 @@ typedef struct button {
 
 /**
  * @fn createButton(sdl_ctx_t *sdl_ctx, button **b, const char *text, SDL_FRect rect, char *baseImgPath, char *hoverImgPath, char *clickImgPath)
- * @param sdl_tx SDL context, passed as a parameter to the function here to load the various button images
- * @param text the text that will appear next to the button
- * @param b a pointer to a button pointer, which will hold the button created when the function is called
- * @param rect the rectangular area where the button will be located
+ * @param sdl_ctx SDL context, passed as a parameter to the function here to load the various \ref button images
+ * @param text the text that will appear next to the \ref button
+ * @param b a pointer to a \ref button pointer, which will hold the \ref button created when the function is called
+ * @param rect the rectangular area where the \ref button will be located
  * @param baseImgPath the path to the base image that needs to be loaded
  * @param hoverImgPath the path to the hovered image that needs to be loaded
  * @param clickImgPath the path to the clicked image that needs to be loaded
  * @return returns a boolean value: true if everything went well, false otherwise
- * @brief allocates memory for a button, initializes its attributes
+ * @brief allocates memory for a \ref button, initializes its attributes
  *
- * allocates memory for a button, initializes its attributes :
+ * allocates memory for a \ref button, initializes its attributes :
  * \ref buttonBox, \ref createRect_Ex is used with rect
  * The images ( \ref baseImg, \ref hoverImg, \ref clickImg) are loaded along with their paths using the IMG_LoadTexture() function
  * The boolean are set to false
@@ -66,34 +53,34 @@ bool createButton(sdl_ctx_t *sdl_ctx, button **b, const char *text, SDL_FRect re
 
 /**
  * @fn updateButtonState(button *b, V2f mouseCoord, int mouseFlag)
- * @param b b is a pointer to the button whose states (Booleans) we want to update
- * @param mouseCoord mouse pointer coordinates in the game window
+ * @param b b is a pointer to the \ref button whose states (Booleans) we want to update
+ * @param mouseCoord mouse pointer coordinates in the game window ( \ref V2f)
  * @param mouseFlag Boolean mask to determine which mouse buttons are pressed
- * @brief This function updates the status booleans of a button
+ * @brief This function updates the status booleans of a \ref button
  *
- * This function updates the status booleans (isHovered, isLeftClicked and isRightClicked) of a button using SDL_PointInRectFloat with mouseCoord
+ * This function updates the status booleans ( \ref isHovered, \ref isLeftClicked and \ref isRightClicked) of a \ref button using SDL_PointInRectFloat with mouseCoord
  * after mouseFlag is compare with SDL MASK to get the mouse input
  */
 void updateButtonState(button *b, V2f mouseCoord, int mouseFlag);
 
 /**
  * @fn renderButton(sdl_ctx_t *sdl_ctx, button *b)
- * @param sdl_tx SDL context, passed as a parameter to the function here to have acces to the renderer whrere the button willbe rendered
- * @param b b is a pointer to the button to print
- * @brief This function displays a button based on its boolean state 
+ * @param sdl_ctx SDL context, passed as a parameter to the function here to have acces to the renderer where the \ref button will be rendered
+ * @param b b is a pointer to the \ref button to print
+ * @brief This function displays a \ref button based on its boolean state 
  *
- * This function displays a button based on its boolean state, with \ref renderImage() and \ref renderText_Ex()
+ * This function displays a \ref button based on its boolean state, with \ref renderImage() and \ref renderText_Ex()
  * If there was a problem loading the image, a rectangle is displayed in its place using \ref renderFillRect(), different colors for each state
  */
 void renderButton(sdl_ctx_t *sdl_ctx, button *b);
 
 /**
  * @fn destroyButton(button **b)
- * @param b a pointer to a button pointer, which is going to be destroyed
- * @brief deletes a button and its properties
+ * @param b a pointer to a \ref button pointer, which is going to be destroyed
+ * @brief deletes a \ref button and its properties
  *
- * deletes a button and its properties with SDl_DestroyTexture() for each images (base, hover and click)
- * free up the previously allocated space, and set to NULL *button.
+ * deletes a button and its properties with SDL_DestroyTexture() for each images (base, hover and click)
+ * free up the previously allocated space, and set to NULL *\ref button.
  */
 void destroyButton(button **b);
 
--- src/checkboxes.h
+++ src/checkboxes.h
@@ -1,32 +1,104 @@
+/**
+ * @file checkboxes.h
+ * @brief File to define what a checkbox is and how it works
+ *
+ * @author Rossignol François <francois_rossignol@outlook.fr>
+ * @date 2026-03-31
+ * @remark last modified 2026-03-31
+ *
+ * Contributors:
+ * Rossignol François <francois_rossignol@outlook.fr>
+ **/
+
 #ifndef CHECKBOXES_H_
 #define CHECKBOXES_H_
 
 #include "common.h"
 
+/**
+ * @struct checkbox
+ * @brief a type that contains all the attributes needed to display a checkbox and handle its interactions (click, hover, checked)
+ *
+ * a checkbox is a two state element that you can check or uncheck
+ */
 typedef struct checkbox {
-    SDL_FRect *checkBox; // forme sur laquelle il va falloir cliquer pour mettre checked a true
-    SDL_FRect *tickBox; // j'ai hésité a créer un FRect pour cela, mais c'est plus pratique, si c'est un symbole '✓' en image,
-                        // comme ça il peut dépasser de la taille de la checkbox
+    SDL_FRect *checkBox; //!< a rectangle that represents the position and dimensions of the rectangle or square that you need to click to check or uncheck a checkbox
+    SDL_FRect *tickBox; //!< Area (position and dimensions) where the checkmark will appear: if it is a ‘✓’ icon, it may extend beyond the checkbox's borders
 
-    SDL_Texture *checkboxImg;
-    SDL_Texture *tickImg;
-    SDL_Texture *tickHoverImg;
-    // image qui est une version 'fantôme' de tick_img, ou n'importe quoi d'autre, dans l'idée cela doit être transparent, cela
-    // s'affiche quand on survole la checkbox et qu'elle n'est pas checked, partage le même box que tickImg
+    SDL_Texture *checkboxImg; //!< the background image of the checkbox (in the \ref checkBox area)
+    SDL_Texture *tickImg; //!< the image of the checkbox tick (in the \ref tickBox area)
+    SDL_Texture *tickHoverImg; //!< a transparent version of the tick icon (it could be something else, but that's what it was designed for) from the checkbox (in the \ref tickBox area as well), displayed when the mouse hovers over the checkbox if it is not \ref checked
 
-    float spaceWithText; // espace entre le texte et le carré/rectangle a cocher
-    float boxBorderSize; // ne sert que quand il n'y a pas d'image
+    float spaceWithText; //!< space between the text and the checkbox (with a ratio already set upon creation)
+    float boxBorderSize; //!< Useful for displaying a border around a checkbox when the image could not be loaded or set
 
-    bool hovered;
-    bool prevClicked;
-    bool clicked;
-    bool checked;
+    bool hovered; //!< a boolean that is true if the mouse hovers over the \ref checkBox
+    bool prevClicked; //!< a boolean value that is true if the \ref checkBox was clicked during the previous iteration of the \ref updateCheckbox function (right-click or left-click)
+    bool clicked; //!< a Boolean value that is currently true, indicating that the \ref updateCheckbox function has detected a click on the \ref checkBox (right-click or left-click)
+    bool checked; //!< a Boolean value indicating whether the checkbox is checked or not
 } checkbox;
 
+
+//note a moi même : rien n'empĉhe le tick box d'être placée a un endroit complètement différent de la checkBox ...
+
+/**
+ * @fn createCheckbox(sdl_ctx_t *sdl_ctx, checkbox **c, SDL_FRect checkRect, SDL_FRect tickRect, char *bgImg_Path, char *tickImg_Path, char *tickHoverImg_Path, float spaceWithText, float boxBorderSize)
+ * @param sdl_ctx SDL context, passed as a parameter to the function here to load the various \ref checkbox images
+ * @param c a pointer to a \ref checkbox pointer, which will hold the \ref checkbox created when the function is called
+ * @param checkRect the rectangular area where the \ref checkbox will be located
+ * @param tickRect the rectangular area where the tick will be located (As I'm reviewing the documentation, I realize that nothing has been done to prevent it from being placed anywhere—unlike the \ref checkbox)
+ * @param bgImg_Path the path to the background image that needs to be loaded (in \ref checkboxImg)
+ * @param tickImg_Path the path to the \ref tickImg image that needs to be loaded (in \ref tickImg)
+ * @param tickHoverImg_Path the path to the \ref tickHoverImg image that needs to be loaded (in \ref tickHoverImg)
+ * @param spaceWithText Used to initialize the \ref spaceWithText attribute; the text in this component does not have a predefined area, only a space with the \ref checkbox
+ * @param boxBorderSize a useful option if there is no image, to ensure the \ref checkbox is clearly visible in that case
+ * @return returns a boolean value: true if everything went well, false otherwise
+ * @brief allocates memory for a \ref checkbox, initializes its attributes (rect, images and boolean  ...)
+ *
+ * allocates memory for a checkbox, initializes its attributes :
+ * \ref checkBox, \ref createRect_Ex is used with checkRect
+ * \ref tickBox, \ref createRect_Ex is used with tickRect
+ * The images ( \ref checkboxImg, \ref tickImg, \ref tickHoverImg) are loaded along with their paths using the IMG_LoadTexture() function
+ * The boolean are set to false by default
+ */
 bool createCheckbox(sdl_ctx_t *sdl_ctx, checkbox **c, SDL_FRect checkRect, SDL_FRect tickRect, char *bgImg_Path,
                     char *tickImg_Path, char *tickHoverImg_Path, float spaceWithText, float boxBorderSize);
+
+/**
+ * @fn destroyCheckbox(checkbox **c)
+ * @param c a pointer to a \ref checkbox pointer, which is going to be destroyed
+ * @brief deletes a \ref checkbox and its properties
+ *
+ * deletes a \ref checkbox and its properties with SDL_DestroyTexture() for each images ( \ref checkboxImg, \ref tickImg and \ref tickHoverImg)
+ * free up the previously allocated space, and set to NULL *\ref checkbox.
+ */                 
 void destroyCheckbox(checkbox **c);
+
+/**
+ * @fn updateCheckboxStates(checkbox *c, V2f mouseCoord, int mouseFlag)
+ * @param c c is a pointer to the \ref checkbox whose states (Booleans) we want to update
+ * @param mouseCoord mouse pointer coordinates in the game window ( \ref V2f)
+ * @param mouseFlag Boolean mask to determine which mouse buttons are pressed
+ * @brief This function updates the status booleans of a \ref checkbox
+ *
+ * This function updates the status booleans ( \ref hovered, \ref prevClicked and \ref clicked and \ref checked) of a \ref checkbox using SDL_PointInRectFloat with mouseCoord
+ * after mouseFlag is compare with SDL MASK to get the mouse input
+ * To toggle the \ref checked state, we detect when the mouse checkbox is released, 
+ * not when it's \ref clicked, because a frame is so short that by the next frame, the checkbox would still be clicked, so timing would be a bit of a gamble.
+ * Hence the usefulness of ` \ref prevClicked`, which lets you know if the click occurred on the previous frame
+ */
 void updateCheckboxStates(checkbox *c, V2f mouseCoord, int mouseFlag);
+
+/**
+ * @fn renderCheckbox(sdl_ctx_t *sdl_ctx, checkbox *c, char *text)
+ * @param sdl_ctx SDL context, passed as a parameter to the function here to have acces to the renderer where the \ref checkbox will be rendered
+ * @param c c is a pointer to the \ref checkbox to print
+ * @param text the text displayed to the right of the \ref checkbox (to the left if \ref spaceWithText was set to a negative value during \ref createCheckbox
+ * @brief This function displays a \ref checkbox based on its boolean state 
+ *
+ * This function displays a \ref checkbox based on its boolean state, with \ref renderImage() and \ref renderText_Ex()
+ * If there was a problem loading the image, there is a fallback with rectangles are displayed in their place using \ref renderFillRect(), different colors for each state
+ */
 void renderCheckbox(sdl_ctx_t *sdl_ctx, checkbox *c, char *text);
 
 #endif // CHECKBOXES_H_
